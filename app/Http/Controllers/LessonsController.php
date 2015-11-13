801ea17563f5c729585801900d2ebb2af097532d
asifbuetcse <asifbuetcse@yahoo.com>
2015-11-13 17:39:20 +0600

    start a lesson

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Category;
+use App\Lesson;
+use App\Lessonbase;
+use App\Question;
+use App\User;
+use App\Answer;
+use DB;
 
 class LessonsController extends Controller
 {
@@ -16,7 +22,7 @@ class LessonsController extends Controller
      */
     public function index()
     {
-        return view('lessons');
+        //
     }
 
     /**
@@ -37,7 +43,35 @@ class LessonsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if (\Auth::check()) {
+            $selectedId = 0;
+            $alreadyLearnedId = 0;
+            $categoryId = $request->input('id');
+            $userId = \Auth::user()->id;
+            $questions = Category::find($categoryId)->questions;
+            foreach ($questions as $index => $question) {
+                $lessonbase = Lessonbase::where('question_id', '=', $question->id)->first();
+                $lesson = Lesson::where('lesson_id', '=' , $lessonbase->lesson_id)->where('user_id', '=', $userId)->first();
+                if (!$lesson) {
+                    $selectedId = $lessonbase->lesson_id;
+                    break;
+                }
+                if ($lesson->score == 0) {
+                    $selectedId = $lesson->lesson_id;
+                    break;
+                }
+                else {
+                    $alreadyLearnedId = $lesson->lesson_id;
+                }
+            }
+            if ($selectedId == 0) {
+                $selectedId = $alreadyLearnedId + 1;
+            }
+            $questionCollection = DB::table('lessonbase')->where('lesson_id', '=', $selectedId)->lists('question_id');
+            $questions = Question::wherein('id', $questionCollection)->get();
+            $answers = Answer::wherein('question_id', $questionCollection)->get();
+            return view('lessons', compact('selectedId', 'questions', 'answers'));
+        }
     }
 
     /**
--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
+    public function category()
+    {
+        return $this->belongsTo('App\Question');
+    }
+    public function lessonbases()
+    {
+        return $this->hasOne('App\Lessonbase');
+    }
 }
